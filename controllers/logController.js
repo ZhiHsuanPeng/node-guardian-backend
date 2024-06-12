e5a116d6859b0d3ece0b4ff9e22e893c10160bce
Jeremy Peng <pengzhihsuan@gmail.com>
2024-06-12 13:01:08 +0800

    Add: set up rabbitMQ to forward incoming data to worker
    	1. Add featrue: when main server accept the incoming req.body
    	   from client, it put the req.body to the job queue.
    	2. Set up worker to consume incoming data.

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,4 +1,18 @@
-exports.insertNewLogs = (req, res) => {
-  console.log('insert');
-  console.log(req.body);
+const amqplib = require('amqplib');
+
+exports.insertNewLogs = async (req, res) => {
+  const amqpUser = process.env.AMQP_USER || 'jeremy';
+  const amqpPassword = process.env.AMQP_PASSWORD || 'jeremy';
+  const serverIp = process.env.AMQP_SERVERIP;
+
+  const rabbitmqServer = `amqp://${amqpUser}:${amqpPassword}@${serverIp}`;
+
+  const queue = 'job';
+  const conn = await amqplib.connect(rabbitmqServer);
+  const ch = await conn.createChannel();
+  await ch.assertQueue(queue);
+
+  ch.sendToQueue(queue, Buffer.from(req.body));
+
+  return res.status(200).json({ message: 'OK' });
 };
---
+++ worker/worker.js
@@ -0,0 +1,26 @@
+const amqplib = require('amqplib');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const amqpUser = process.env.AMQP_USER || 'jeremy';
+const amqpPassword = process.env.AMQP_PASSWORD || 'jeremy';
+const serverIp = process.env.AMQP_SERVERIP;
+
+const rabbitmqServer = `amqp://${amqpUser}:${amqpPassword}@${serverIp}`;
+
+(async () => {
+  const queue = 'job';
+  const conn = await amqplib.connect(rabbitmqServer);
+  const ch = await conn.createChannel();
+  await ch.assertQueue(queue);
+
+  ch.consume(queue, (msg) => {
+    if (msg !== null) {
+      console.log('Received:', msg.content.toString());
+      ch.ack(msg);
+    } else {
+      console.log('Consumer cancelled by server');
+    }
+  });
+})();
