d55bab3db92e87f3a9b7dbd6e68700ad5ec39537
Jeremy Peng <pengzhihsuan@gmail.com>
2024-06-14 10:59:13 +0800

    Modified and add: RDS pool configuration and OS client
    	1. Due to future need to connect and use RDS, I now creat
    	   models_RDS folder and set up poolConnection to prepare for
    	   future usage.
    	2. I refactor openSearch client from utils to another model folder.`

---
+++ models_RDS/databasePool.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
+
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+module.exports = pool;
--- models_openSearch/openSearch.js
+++ models_openSearch/openSearch.js
@@ -1,13 +1,16 @@
 const { Client } = require('@opensearch-project/opensearch');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '../.env' });
+dotenv.config();
+
+const username = process.env.OPENSEARCH_USER;
+const password = process.env.OPENSEARCH_PASS;
 
 const openSearchClient = new Client({
   node: 'https://search-errorlog-sjbarfvt6la3sff7iuqqkov6x4.ap-southeast-2.es.amazonaws.com',
   auth: {
-    username: process.env.OPENSEARCH_USER,
-    password: process.env.OPENSEARCH_PASS,
+    username,
+    password,
   },
 });
 
