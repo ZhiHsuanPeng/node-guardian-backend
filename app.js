aeda3d651ff7d110a6ffc01879a858f6d0d0e223
Jeremy Peng <pengzhihsuan@gmail.com>
2024-06-17 21:32:56 +0800

    Install argon2 to hash user password.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 const logRouter = require('./routers/logRouter');
 const viewRouter = require('./routers/viewRouter');
@@ -15,6 +16,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 app.use('/', viewRouter);
 app.use('/api/v1/logs', logRouter);
--- models_RDS/user.js
+++ models_RDS/user.js
@@ -1,10 +1,12 @@
 const pool = require('./databasePool');
+const argon2 = require('argon2');
 
 exports.createUser = async (name, email, password) => {
+  const hashedPassword = await argon2.hash(password);
   const results = await pool.query(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [
     name,
     email,
-    password,
+    hashedPassword,
   ]);
   if (results[0].insertId) {
     return results[0].insertId;
