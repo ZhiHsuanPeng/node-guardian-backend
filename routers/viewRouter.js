360a780292e2d492b7509c29cd865dbe7cefd09c
Jeremy Peng <pengzhihsuan@gmail.com>
2024-06-18 16:14:26 +0800

    Change end point name to lowercase.

--- routers/viewRouter.js
+++ routers/viewRouter.js
@@ -4,8 +4,8 @@ const authenticate = require('../middleware/authenticate');
 
 const router = express.Router();
 
-router.route('/signUp').get(viewController.renderSignUpForm);
-router.route('/signIn').get(viewController.renderSignInForm);
+router.route('/signup').get(viewController.renderSignUpForm);
+router.route('/signin').get(viewController.renderSignInForm);
 
 router.route('/a/:accountName').get(authenticate, viewController.renderOverViewPage);
 router.route('/a/:accountName/prj/:prjName').get(authenticate, viewController.renderBasicProjectPage);
