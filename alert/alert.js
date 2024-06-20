955768c788b8fc083094e992a35af68e2cda4f95
Jeremy Peng <pengzhihsuan@gmail.com>
2024-06-20 14:06:09 +0800

    Remove: remove check first logic from alert worker
    	1. Sine alert worker does not need to check if the incoming
    	   error logs is the first, I removed the corresponding code.

--- alert/alert.js
+++ alert/alert.js
@@ -11,50 +11,6 @@ const amqpPassword = process.env.AMQP_PASSWORD;
 const serverIp = process.env.AMQP_SERVERIP;
 const rabbitmqServer = `amqp://${amqpUser}:${amqpPassword}@${serverIp}`;
 
-const checkIsFirstAndSetAlert = async (payLoad) => {
-  try {
-    // Search for error message in search engine
-    const checkIsFirstError = await client.search({
-      index: payLoad.accessToken,
-      body: {
-        size: 100,
-        query: {
-          bool: {
-            must: [
-              {
-                term: {
-                  'errMessage.keyword': payLoad.errMessage,
-                },
-              },
-            ],
-          },
-        },
-      },
-    });
-    console.log(checkIsFirstError.hits.total);
-    const docNum = checkIsFirstError.hits.total.value;
-    // If the result is not 0, it indicates that this is not the first occurrence
-    console.log(docNum);
-    if (!checkIsFirstError.hits.total.value * 1 === 1) {
-      // Handle situation when this error is not the first
-      // Use redis to increment error count
-      console.log('not first');
-    }
-    const [rows] = await pool.query(
-      'select u.email, p.alertFirst from projects AS p INNER JOIN access AS a ON p.id = a.projectId INNER JOIN users AS u ON u.id = a.userId WHERE p.token = ?',
-      [payLoad.accessToken]
-    );
-    console.log(rows);
-
-    // if (project.alertFirst === 'on') {
-    //   mail.sendFirstErrorEmail();
-    // }
-  } catch (error) {
-    console.error('Error querying the database:', error);
-    return false;
-  }
-};
-
 (async () => {
   const queue = 'alert';
   const conn = await amqplib.connect(rabbitmqServer);
@@ -64,7 +20,7 @@ const checkIsFirstAndSetAlert = async (payLoad) => {
   ch.consume(queue, async (msg) => {
     if (msg !== null) {
       const payLoad = JSON.parse(msg.content.toString());
-      // await checkIsFirstAndSetAlert(payLoad);
+
       console.log('Alert worker just process one alert');
       ch.ack(msg);
     } else {
