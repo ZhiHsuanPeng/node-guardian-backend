d351d705b86ad25076b622f06bdc598bb5318552
Jeremy Peng <pengzhihsuan@gmail.com>
2024-07-06 22:25:26 +0800

    Complete styling the first section of the home page.

---
+++ public/js/home.js
@@ -0,0 +1,10 @@
+const tryFree = document.querySelector('.tryFreeBtn');
+const tryForFree = document.querySelector('.tryForFreeBtn');
+
+tryFree.addEventListener('click', () => {
+  window.location.href = '/signup';
+});
+
+tryForFree.addEventListener('click', () => {
+  window.location.href = '/signup';
+});
