903ca0cdbf0f024f03cff9ed413e8ccfdfe22412
Jeremy Peng <pengzhihsuan@gmail.com>
2024-07-07 15:01:01 +0800

    Add navbar style and add the second section of product description.

--- public/js/home.js
+++ public/js/home.js
@@ -13,3 +13,20 @@ tryForFree.addEventListener('click', () => {
 signIn.addEventListener('click', () => {
   window.location.href = '/signin';
 });
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.header');
+  const logo = document.querySelector('.logo');
+  const name = document.querySelector('.project_name');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+    logo.setAttribute('src', '/img/icon_blue_big.png');
+    name.style.color = '#272f40';
+    name.style.fontSize = '24px';
+  } else {
+    header.classList.remove('scrolled');
+    logo.setAttribute('src', '/img/icon_big.png');
+    name.style.color = 'white';
+    name.style.fontSize = '28px';
+  }
+});
