317eb60549c51cfc2302f540d0bad294b3c58263
Jeremy Peng <pengzhihsuan@gmail.com>
2024-07-22 15:04:47 +0800

    Set the worker to connect to new self managed elasticsearch

--- services/worker/worker.js
+++ services/worker/worker.js
@@ -6,10 +6,21 @@ const mail = require('../../utils/mail');
 
 dotenv.config();
 
+// const client = new Client({
+//   node: process.env.ELASTICSEARCH_NODE,
+//   auth: {
+//     apiKey: process.env.ELASTICSEARCH_APIKEY,
+//   },
+// });
+
 const client = new Client({
-  node: process.env.ELASTICSEARCH_NODE,
+  node: process.env.ELASTIC_IP,
   auth: {
-    apiKey: process.env.ELASTICSEARCH_APIKEY,
+    username: process.env.ELASTIC_USER,
+    password: process.env.ELASTIC_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
